65490310287b284d7ff3cbe4d79504e10090ce91
Evan Phelps <evan.phelps@gmail.com>
2013-05-06 13:15:44 -0400

    pull out sieve of erat to nttools added p11 and p12

--- solutions.py
+++ solutions.py
@@ -7,12 +7,14 @@ import math
 from itertools import takewhile
 from itertools import repeat
 from itertools import tee
+from itertools import combinations
 import operator as op
 from functools import reduce
 from collections import Counter
 try: import numpypy # for pypy experimental support
 except ImportError: pass
 import numpy
+import nttools
 
 # in case of command-line timeit, set stdoutON = False, i.e., from bash
 # python -mtimeit -s "import solutions as pe"\
@@ -41,22 +43,6 @@ def part(l, N):
     """partition list 'l' into sublists of length 'N'"""
     return [l[n:n+N] for n in range(0,len(l),N)]
 
-def pfactorGen(N):
-    """generate prime factors of the number N"""
-    i = 2
-    n = N
-    # divide out the lowest numbers first so that as long as the
-    # reduced n is composite, it must be greater than the square of the
-    # next largest number (n>i^2).
-    while i*i <= n:
-        while n%i == 0:
-            yield i      # n is divisible by i
-            n /= i
-        i += 1
-    # the final reduced n is the last and largest non-composite (prime)
-    # factor of N.
-    yield int(n)
-
 def sieveErat(N=10):
     # should probably be implemented as map to allow for quick removal
     # of eliminated numbers rather than redundantly setting the
@@ -74,6 +60,35 @@ def sieveErat(N=10):
         for j in range(i+v,N-1,v): nums[j][1] = False
         i = getnext(i+1,nums)
     return [n[0] for n in nums if n[1]]
+
+def factorGen(N):
+    """generate all factors of the number N"""
+
+    factors = []
+    for x in range(2, int(math.sqrt(N))):
+        d,r = divmod(N,x)
+        if r == 0:
+            factors.append(x)
+            factors.append(d)
+ 
+    return [1, N] + factors
+
+def pfactorGen(N):
+    """generate prime factors of the number N"""
+    n = N
+    p = list(takewhile(lambda x: x < N, nttools.gen_primes()))
+    i = 0
+    # divide out the lowest numbers first so that as long as the
+    # reduced n is composite, it must be greater than the square of the
+    # next largest number (n>i^2).
+    while p[i]*p[i] <= n:
+        while n%p[i] == 0:
+            yield p[i]      # n is divisible by i
+            n /= p[i]
+        i += 1
+    # the final reduced n is the last and largest non-composite (prime)
+    # factor of N.
+    if n > 1: yield int(n)
 
 # #####################################################################
 # ######################## SOLUTION FUNCTIONS #########################
@@ -214,6 +229,16 @@ def p7(nth=10001):
     return list(sieveErat(int(N)))[nth-1]
 
 @timeit
+def p7alt(N=10001):
+    """What is the 10001st prime number?"""
+    # currently, from command line, need to get 100001st element from
+    # returned list. need to implement generator for this instead!
+    # for now, I'll estimate how many to pull based on the prime
+    # number theorem.
+    return list(takewhile(lambda x: x[0]<N,enumerate(nttools.gen_primes())))[N-1]
+    #return list(sieveErat(int(N)))[nth-1]
+
+@timeit
 def p8():
     """Find the greatest product of five consecutive digits in the
     1000-digit number.
@@ -236,11 +261,76 @@ def p9():
             c = pow(a**2+b**2,0.5)
             if a+b+c==1000:
                 return a*b*c
+
 @timeit
 def p10(N=10):
     """Find the sum of all the primes below two million."""
     return sum(sieveErat(N))
-    
+
+@timeit
+def p11():
+    """What is the greatest product of four adjacent numbers in the same
+    direction (up, down, left, right, or diagonally) in the 2020 grid?
+    46
+    """
+    grid = [['08','02','22','97','38','15','00','40','00','75','04','05','07','78','52','12','50','77','91','08'],
+            ['49','49','99','40','17','81','18','57','60','87','17','40','98','43','69','48','04','56','62','00'],
+            ['81','49','31','73','55','79','14','29','93','71','40','67','53','88','30','03','49','13','36','65'],
+            ['52','70','95','23','04','60','11','42','69','24','68','56','01','32','56','71','37','02','36','91'],
+            ['22','31','16','71','51','67','63','89','41','92','36','54','22','40','40','28','66','33','13','80'],
+            ['24','47','32','60','99','03','45','02','44','75','33','53','78','36','84','20','35','17','12','50'],
+            ['32','98','81','28','64','23','67','10','26','38','40','67','59','54','70','66','18','38','64','70'],
+            ['67','26','20','68','02','62','12','20','95','63','94','39','63','08','40','91','66','49','94','21'],
+            ['24','55','58','05','66','73','99','26','97','17','78','78','96','83','14','88','34','89','63','72'],
+            ['21','36','23','09','75','00','76','44','20','45','35','14','00','61','33','97','34','31','33','95'],
+            ['78','17','53','28','22','75','31','67','15','94','03','80','04','62','16','14','09','53','56','92'],
+            ['16','39','05','42','96','35','31','47','55','58','88','24','00','17','54','24','36','29','85','57'],
+            ['86','56','00','48','35','71','89','07','05','44','44','37','44','60','21','58','51','54','17','58'],
+            ['19','80','81','68','05','94','47','69','28','73','92','13','86','52','17','77','04','89','55','40'],
+            ['04','52','08','83','97','35','99','16','07','97','57','32','16','26','26','79','33','27','98','66'],
+            ['88','36','68','87','57','62','20','72','03','46','33','67','46','55','12','32','63','93','53','69'],
+            ['04','42','16','73','38','25','39','11','24','94','72','18','08','46','29','32','40','62','76','36'],
+            ['20','69','36','41','72','30','23','88','34','62','99','69','82','67','59','85','74','04','36','16'],
+            ['20','73','35','29','78','31','90','01','74','31','49','71','48','86','81','16','23','57','05','54'],
+            ['01','70','54','71','83','51','54','69','16','92','33','48','61','43','52','01','89','19','67','48']]
+    nrows = len(grid)
+    ncols = len(grid[0])
+    maxprod = 0
+    #ugly solution
+    for i in range(0,nrows):
+        for j in range(0,ncols):
+            if j < ncols-4:
+                val = 1
+                for v in [int(grid[i][j+j1]) for j1 in range(0,4)]:  val *= v
+                if val > maxprod: maxprod = val
+            if i < nrows-4:
+                val = 1
+                for v in [int(grid[i+i1][j]) for i1 in range(0,4)]:  val *= v
+                if val > maxprod: maxprod = val
+            if i < nrows-4 and j < ncols-4:
+                val = 1
+                for v in [int(grid[i+dinc][j+dinc]) for dinc in range(0,4)]:  val *= v
+                if val > maxprod: maxprod = val
+            if i > 2 and j < ncols-4:
+                val = 1
+                for v in [int(grid[i-dinc][j+dinc]) for dinc in range(0,4)]:  val *= v
+                if val > maxprod: maxprod = val
+    return maxprod
+
+@timeit
+def p12(N=5,N0=1):
+    """What is the value of the first triangle number to have over five
+    hundred divisors?
+    """
+    n = N0
+    nfs = 1
+    for i,n in [(i,i*(i+1)/2) for i in range(N0,N+1)]:
+        nfs = len(factorGen(n))
+        if nfs >= 500:
+            print(i,nfs,int(n))
+            break
+    return int(n)
+
 # #####################################################################
 # ################# PRELIMINARY ANALYSIS FUNCTIONS ####################
 # #####################################################################
@@ -257,3 +347,5 @@ def p1_ana():
     seqstrlen = len(str(toadd2[0]))
     for n7 in toadd2:
         print(str("%%+%ds"%(msglen-(msglen-seqstrlen+2)/2)) % str(n7))
+
+    
